972bd53f08111f5711d8af7bdf16f5b7c7bc30cb
Pierre <jonkerpierre637@gmail.com>
2024-04-11 14:05:19 +0200

    Unit 5 Done

--- My_NodeJS_Work/unit_5/lesson_24/recipe_app_24_3/main.js
+++ My_NodeJS_Work/unit_5/lesson_24/recipe_app_24_3/main.js
@@ -88,8 +88,7 @@ router.post(
   usersController.validate,
   usersController.create,
   usersController.redirectView
-);
-router.get("/users/login", usersController.login);
+)
 router.get("/users/login", usersController.login);
 router.post("/users/login", usersController.authenticate, usersController.redirectView);
 router.get("/users/logout", usersController.logout, usersController.redirectView);
