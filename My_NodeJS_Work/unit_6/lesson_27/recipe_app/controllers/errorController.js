833fce68741c89e011598fcc5aacea574000f684
Pierre <jonkerpierre637@gmail.com>
2024-04-12 12:36:39 +0200

    Lesson 27 Done

--- My_NodeJS_Work/unit_6/lesson_27/recipe_app/controllers/errorController.js
+++ My_NodeJS_Work/unit_6/lesson_27/recipe_app/controllers/errorController.js
@@ -10,7 +10,10 @@ exports.logErrors = (error, req, res, next) => {
 exports.respondNoResourceFound = (req, res) => {
   let errorCode = httpStatus.NOT_FOUND;
   res.status(errorCode);
-  res.send(`${errorCode} | The page does not exist!`);
+  res.send(`./public${errorCode}.html`, {
+  root: "./"
+  });
+   
 };
 
 exports.respondInternalError = (error, req, res, next) => {
